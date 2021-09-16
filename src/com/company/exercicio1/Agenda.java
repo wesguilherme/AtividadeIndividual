7c7ad0b47eb67b003227b86554a274e7e9b0e928
Wesley Guilherme Alves <wesley.guilherme@mercadolivre.com>
2021-09-16 14:23:14 -0300

    update

--- src/com/company/exercicio1/Agenda.java
+++ src/com/company/exercicio1/Agenda.java
@@ -1,4 +1,29 @@
 package com.company.exercicio1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Agenda {
+
+    //armazena pessoas
+    List<Pessoa> pessoa = new ArrayList<>();
+
+    //metodo armazenar pessoas
+    public void armazenarPessoa(Pessoa pessoa){
+        this.pessoa.add(pessoa);
+    }
+
+    public void removerPessoa(String nome){
+
+    }
+
+    public int buscarPessoa(String nome){
+
+    }
+
+    public void exibirTodaAgenda(){
+
+    }
+
+    public void exibirPessoa(int index){}
 }
